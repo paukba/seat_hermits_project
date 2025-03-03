6de0b81891065155fbd09fa4d8a3ed174d84648c
Benjamin Paukovitz <benjamin.a.paukovitz@hotmail.com>
2025-03-03 03:14:45 -0500

    Added Song.java class

---
+++ src/main/java/com/model/Song.java
@@ -0,0 +1,89 @@
+package com.model;
+
+import java.util.UUID;
+import java.util.ArrayList;
+
+public class Song {
+    public UUID id;
+    private ArrayList<Tab> music;
+    private String title;
+    private Author author;
+    private boolean privacy;
+    private Genre genre;
+    private ArrayList<String> lyrics;
+    private ArrayList<Instrument> instruments;
+    private ArrayList<Measure> measures;
+    private ArrayList<NotationFormat> notations;
+    private ArrayList<Sheet> sheets;
+    private ArrayList<Tab> tabs;
+    private ArrayList<String> comments;
+
+    public Song(String name, ArrayList<Tab> music, ArrayList<String> lyrics, Genre genre, bool privacy, Instrument instrument) {
+        
+    }
+
+    public String getTitle() {
+
+    }
+
+    public ArrayList<Sheet> getSheets(Instrument instrument) {
+        return sheets;
+    }
+
+    public ArrayList<Tab> getTabs(Instrument instrument) {
+        return tabs;
+    }
+
+    public ArrayList<String> getLyrics() {
+        
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public ArrayList<Instrument> getInstruments() {
+
+    }
+
+    public boolean isPrivate() {
+
+    }
+
+    public void setTitle(String name) {
+
+    }
+
+    public void setSheets(ArrayList<Sheet> sheets) {
+
+    }
+
+    public void setTabs(ArrayList<Tab> tabs) {
+
+    }
+
+    public void setLyrics(ArrayList<String> lyrics) {
+
+    }
+
+    public void setGenre(Genre genre) {
+
+    }
+
+    public void setPrivacy(boolean privacy) {
+        
+    }
+
+    public void playSong(Song song) {
+
+    }
+
+    public void addComment(String comment) {
+
+    }
+
+    public void removeComment(String comment) {
+
+    }
+
+}
