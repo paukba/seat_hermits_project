01b7cc053ffcb97e64d102d7b589637d31a3fa49
thomasrz <thomaszwetsloot@icloud.com>
2025-03-04 14:05:12 -0500

    added user constants to DataConstants

---
+++ src/main/java/com/model/DataConstants.java
@@ -0,0 +1,19 @@
+package com.model;
+
+public abstract class DataConstants {
+    protected static final String USER_FILE_NAME = "json/users.json";
+    protected static final String USER_ID = "id";
+    protected static final String USER_TYPE = "type";
+    protected static final String USER_FIRST_NAME = "firstName";
+    protected static final String USER_LAST_NAME = "lastName";
+    protected static final String USER_USER_NAME = "userName";
+    protected static final String USER_EMAIL = "email";
+    protected static final String USER_PASSWORD = "password";
+    protected static final String USER_FAV_SONGS = "favsongs-id";
+    protected static final String USER_MY_SONGS = "mysongs-id";
+
+    protected static final String INSTRUMENT_FILE_NAME = "json/instrument.json";
+    protected static final String INSTRUMENT_NAME = "name";
+
+    protected static final String SONG_FILE_NAME = "json/songs.json";
+}
