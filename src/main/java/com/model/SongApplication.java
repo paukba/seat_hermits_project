89a7c40eb48ffa5b5e70a4a470c518de14d6ff88
MCooperM05 <coopmur7@hotmail.com>
2025-03-04 13:58:13 -0500

    added SongApplication.java with stubs

---
+++ src/main/java/com/model/SongApplication.java
@@ -0,0 +1,67 @@
+package com.model;
+
+import java.util.ArrayList;
+
+import javafx.scene.control.Tab;
+
+public class SongApplication {
+    private SongList songList;
+    private UserList userLiser;
+    private User user;
+
+    public SongApplication() {
+
+    }
+
+    public ArrayList<Song> findSongs() {
+        return new ArrayList<Song>();
+    }
+
+    public ArrayList<Song> findSongs(String keyword) {
+        return new ArrayList<Song>();
+    }
+
+    public User createAccount(String firstName, String lastName, String email, String userName) {
+        return new User(firstName, lastName, email, userName);
+    }
+
+    public User login(String userName, String password) {
+        return new User(password, password, password, userName);
+    }
+
+    public ArrayList<Song> getFavoriteSongs() {
+        return new ArrayList<Song>();
+    }
+
+    public ArrayList<Song> getMySongs() {
+        return new ArrayList<Song>();
+    }
+
+    public void addSong(String name, ArrayList<Tab> music, ArrayList<String> lyrics, Genre genre, boolean privacy, ArrayList<Instrument> instruments) {
+
+    }
+
+    public void editSongTitle(String songID, String name) {
+
+    }
+
+    public void editSongSheets(String songID, ArrayList<Sheet> sheets) {
+        
+    }
+
+    public void editSongTabs(String songID, ArrayList<Tab> tabs) {
+        
+    }
+
+    public void editSongLyrics(String songID, ArrayList<String> lyrics) {
+        
+    }
+
+    public void editSongPrivacy(String songID, boolean privacy) {
+        
+    }
+
+    public void deleteSong(String songID) {
+        
+    }
+}
