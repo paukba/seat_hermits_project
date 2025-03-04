e18b813cd3d6e74de373a08fe52eb554a8b1dafe
Benjamin Paukovitz <benjamin.a.paukovitz@hotmail.com>
2025-03-04 14:04:48 -0500

    Added NotationFormat.java, Sheet.java, and Tab.java with stubs

---
+++ src/main/java/com/model/Tab.java
@@ -0,0 +1,21 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class Tab implements NotationFormat{
+    private ArrayList<TabNote> tabNotes;
+    private Instrument instrument;
+
+    public Tab(ArrayList<TabNote> tabNotes) {
+
+    }
+
+    public void displayTab() {
+
+    }
+
+    @Override
+    public void displayFormat() {
+        
+    }
+}
