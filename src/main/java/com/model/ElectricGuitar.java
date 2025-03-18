e3f1c927eb9bf7df00af3587ba562c9395c1426f
MCooperM05 <coopmur7@hotmail.com>
2025-03-18 14:32:04 -0400

    added functionality to ElectricGuitar

--- src/main/java/com/model/ElectricGuitar.java
+++ src/main/java/com/model/ElectricGuitar.java
@@ -1,5 +1,30 @@
 package com.model;
 
-public class ElectricGuitar {
-    
+import java.util.ArrayList;
+
+public class ElectricGuitar extends Instrument {
+    private String name;
+    private ArrayList<Note> notes;
+    private ArrayList<Chord> chords;
+    private static final int NUM_STRINGS = 6;
+
+    public ElectricGuitar(String name, ArrayList<Note> notes, ArrayList<Chord> chords) {
+        super(name, notes, chords);
+    }
+
+    public ArrayList<Note> getNotes() {
+        return notes;
+    }
+
+    public ArrayList<Chord> getChord() {
+        return chords;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumStrings() {
+        return NUM_STRINGS;
+    }
 }
