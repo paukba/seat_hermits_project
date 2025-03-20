e0829a60d962f24bee706487859fa4b66f6a38c8
Benjamin Paukovitz <benjamin.a.paukovitz@hotmail.com>
2025-03-20 13:52:52 -0400

    Changed Instrument.java to be an interface and ElectricGuitar to implement it

--- src/main/java/com/model/ElectricGuitar.java
+++ src/main/java/com/model/ElectricGuitar.java
@@ -2,14 +2,17 @@ package com.model;
 
 import java.util.ArrayList;
 
-public class ElectricGuitar extends Instrument {
+public class ElectricGuitar implements Instrument {
     private String name;
     private ArrayList<Note> notes;
     private ArrayList<Chord> chords;
     private static final int NUM_STRINGS = 6;
 
     public ElectricGuitar(String name, ArrayList<Note> notes, ArrayList<Chord> chords) {
-        super(name, notes, chords);
+        // may not be correct
+        this.name = name;
+        this.notes = notes;
+        this.chords = chords;
     }
 
     public ArrayList<Note> getNotes() {
--- src/main/java/com/model/Instrument.java
+++ src/main/java/com/model/Instrument.java
@@ -2,27 +2,10 @@ package com.model;
 
 import java.util.ArrayList;
 
-public abstract class Instrument {
-    private String name;
-    private ArrayList<Note> notes;
-    private ArrayList<Chord> chords;
-    private boolean hasStrings;
+public interface Instrument {
+    public ArrayList<Note> getNotes();
 
-    public Instrument(String name, ArrayList<Note> notes, ArrayList<Chord> chords) {
-        this.name = name;
-        this.notes = notes;
-        this.chords = chords;
-    }
+    public ArrayList<Chord> getChord();
 
-    public ArrayList<Note> getNotes() {
-        return notes;
-    }
-
-    public ArrayList<Chord> getChord() {
-        return chords;
-    }
-
-    public String getName() {
-        return name;
-    }
+    public String getName();
 }
