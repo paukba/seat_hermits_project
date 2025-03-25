d32ef95ff3cace1f3355a831b2ed83b0955d1d5b
MCooperM05 <coopmur7@hotmail.com>
2025-03-25 14:56:24 -0400

    edited userList; tested login with correct user/password; tested login with wrong user/password

--- src/main/java/com/model/Driver.java
+++ src/main/java/com/model/Driver.java
@@ -2,6 +2,26 @@ package com.model;
 
 public class Driver {
     public static void main(String[] args) {
-        // implement Song and SongList
+        UserList userList = UserList.getInstance();
+        
+        // login correct
+        User correctLogin = userList.login("iamSpider99", "spiderman2099roolz");
+
+        if (correctLogin != null) {
+            System.out.println(correctLogin.getId() + ": " + correctLogin.getFirstName() + " " + correctLogin.getLastName());
+        } else {
+            System.out.println("Failed to login");
+        }
+
+
+
+        // login wrong
+        User wrongLogin = userList.login("iamSpider", "spidermanroolz");
+
+        if (wrongLogin != null) {
+            System.out.println(wrongLogin.getId() + ": " + wrongLogin.getFirstName() + " " + wrongLogin.getLastName());
+        } else {
+            System.out.println("Failed to login");
+        }
     }
 }
