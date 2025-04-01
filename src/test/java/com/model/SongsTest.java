02c58841f26a1cd0b174a71907c91e127fc297c2
Benjamin Paukovitz <benjamin.a.paukovitz@hotmail.com>
2025-04-01 14:15:56 -0400

    Added (mostly empty) file for testing songs

---
+++ src/test/java/com/model/SongsTest.java
@@ -0,0 +1,26 @@
+package com.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SongsTest {
+    
+    @Test
+    public void testValidSong() {
+        Song testSong = new Song(null, null, null, null, null, null, null, 0, null);
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+
+    @Test
+    public void test3() {
+        
+    }
+}
