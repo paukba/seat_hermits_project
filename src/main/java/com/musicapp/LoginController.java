02a6b777e4d48c4e241fd0ac3a64c8d45accdc34
thomasrz <thomaszwetsloot@icloud.com>
2025-04-15 14:31:56 -0400

    added login button functionality

--- src/main/java/com/musicapp/LoginController.java
+++ src/main/java/com/musicapp/LoginController.java
@@ -5,7 +5,15 @@ import javafx.fxml.FXML;
 
 public class LoginController {
 
-    
+    @FXML
+    private void switchToLoggedIn() throws IOException {
+        App.setRoot("loggedin");
+    }
+
+    @FXML
+    private void switchToCreateAccount() throws IOException {
+        App.setRoot("createaccount");
+    }
 
     
 }
